f4c0ef978bc7b02ca760ae16f3b09daea1c11360
hoshicasia <hoshicasia@gmail.com>
2025-11-22 15:37:44 +0300

    Deleted unused model

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,5 +1,4 @@
 from src.model.baseline_model import BaselineModel
-from src.model.convtasnet import SimpleConvTasNet
 from src.model.rtfsnet import RTFSNet
 
-__all__ = ["BaselineModel", "SimpleConvTasNet", "RTFSNet"]
+__all__ = ["BaselineModel", "RTFSNet"]
