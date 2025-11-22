bd59d41fbb83775d39e3cf9e2f234950f9c3e5f9
hoshicasia <hoshicasia@gmail.com>
2025-11-22 16:44:52 +0300

    Updated final configs for inference

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,4 +1,4 @@
 HYDRA_FULL_ERROR=1 uv run python3 inference.py \
     datasets.test.data_dir=inference_dataset \
-    inferencer.from_pretrained=saved/testing/model_best.pth \
-    model.use_video=True
+    inferencer.from_pretrained=rtfs/model_best.pth \
+    datasets=custom_dir
